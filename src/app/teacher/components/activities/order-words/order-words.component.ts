af1b2d37250f616024b87d957c5e5e4d1e3d93a3
Jordanfvc26 <jverac12@uteq.edu.ec>
2024-02-14 23:44:13 -0500

    Registrar pregunta de tipo ordenar palabras

--- src/app/teacher/components/activities/order-words/order-words.component.ts
+++ src/app/teacher/components/activities/order-words/order-words.component.ts
@@ -1,8 +1,13 @@
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SpinnerComponent } from '../../../../shared-components/spinner/spinner.component';
-import { FormControl, FormRecord, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormRecord, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { ApiResponseRegisterQuestionIT, BodyRegisterQuestionIT } from '../../../interfaces/activities.interface';
+import { CommonModule } from '@angular/common';
+import { ActivitiesService } from '../../../services/activities.service';
+import { Router } from '@angular/router';
+import { ToastAlertsService } from '../../../../shared-components/services/toast-alerts.service';
 import * as iconos from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,7 +17,12 @@ import * as iconos from '@fortawesome/free-solid-svg-icons';
     FontAwesomeModule,
     SpinnerComponent,
     ReactiveFormsModule,
-    NzFormModule
+    NzFormModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [
+    ActivitiesService
   ],
   templateUrl: './order-words.component.html',
   styleUrls: ['./order-words.component.css', '../single-select/single-select.component.css']
@@ -20,28 +30,59 @@ import * as iconos from '@fortawesome/free-solid-svg-icons';
 export class OrderWordsComponent {
 
   //Variables
+  @Input() moduleId: number = 0;
   spinnerStatus: boolean = false;
   validateForm: FormRecord<FormControl<string>> = this.fb.record({});
   listOfControl: Array<{ id: number; controlInstance: string }> = [];
+  questionForm!: FormGroup;
+  limit: number = 0;
+  tidyOptions: string[] = [];
+  messyOptions: string[] = [];
 
   //constructor
   constructor(
-    private fb: NonNullableFormBuilder
+    private fb: NonNullableFormBuilder,
+    private formBuilder: FormBuilder,
+    private activitiesService: ActivitiesService,
+    private toastr: ToastAlertsService,
+    private router: Router
   ) { }
 
   //ngOnInit
   ngOnInit(): void {
     this.spinnerStatus = true;
+    this.createQuestionForm();
     this.addField();
+  }
+
+  //Método que obtiene los headers
+  getHeaders() {
+    let headers = new Map();
+    headers.set("token", sessionStorage.getItem("token"));
+    headers.set("typeUser", sessionStorage.getItem("typeUser"));
+    return headers;
+  }
+
+  //Método que crea el formulario para crear un módulo
+  createQuestionForm() {
+    this.questionForm = this.formBuilder.group({
+      difficulty: ['',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.maxLength(2)
+        ]
+      ],
+    });
   }
 
   //Método que agrega las filas
   addField(e?: MouseEvent): void {
     e?.preventDefault();
-    const id = this.listOfControl.length > 0 ? this.listOfControl[this.listOfControl.length - 1].id + 1 : 0;
-    const controlInstance = `option ${id}`;
+    this.limit = this.listOfControl.length > 0 ? this.listOfControl[this.listOfControl.length - 1].id + 1 : 0;
+    const controlInstance = `option ${this.limit}`;
     const control = {
-      id,
+      id: this.limit,
       controlInstance
     };
     this.listOfControl.push(control);
@@ -49,6 +90,7 @@ export class OrderWordsComponent {
       controlInstance,
       this.fb.control('', Validators.required)
     );
+    this.limit = this.listOfControl.length >= 5 ? 5 : this.listOfControl.length - 1;
   }
 
   //Método que elimina las filas
@@ -58,16 +100,58 @@ export class OrderWordsComponent {
       const index = this.listOfControl.indexOf(i);
       this.listOfControl.splice(index, 1);
       this.validateForm.removeControl(i.controlInstance);
+      this.limit = this.listOfControl.length >= 5 ? 5 : this.listOfControl.length - 1;
     }
+  }
+
+  //Método que desordena el array
+  shuffleArray<T>(array: T[]) {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
   }
 
   //Método que manda a registrar la pregunta
   registerQuestion() {
-    //Aquí se obtiene el array con las opciones
-    const formValues = this.listOfControl.map(control => this.validateForm.get(control.controlInstance)?.value);
-    console.log(formValues);
-  }
+    this.tidyOptions = this.listOfControl.map(control => this.validateForm.get(control.controlInstance)?.value);
+    this.messyOptions = this.shuffleArray(this.tidyOptions);
 
+    this.spinnerStatus = false;
+    let body: BodyRegisterQuestionIT = {
+      text_root: "Ordena las siguientes palabras para formar una oración que tenga sentido:",
+      difficulty: this.questionForm.get('difficulty')?.value,
+      type_question: "order_word",
+      options: {
+        select_mode: "",
+        text_options: this.messyOptions,
+        text_to_complete: "",
+        hind: ""
+      },
+      correct_answer: {
+        true_or_false: false,
+        text_options: this.tidyOptions,
+        text_to_complete: []
+      }
+    }
+    this.activitiesService.registerQuestion(this.getHeaders(), body, this.moduleId)
+      .subscribe({
+        next: (data: ApiResponseRegisterQuestionIT) => {
+          if (data.id != 0) {
+            this.spinnerStatus = true;
+            this.questionForm.reset();
+            this.toastr.showToastSuccess("Pregunta registrada correctamente", "Éxito");
+            this.router.navigateByUrl("/teacher/home/activities/list-activities");
+          }
+        },
+        error: (error: any) => {
+          this.spinnerStatus = true;
+          this.toastr.showToastError("Error", "No se pudo registrar su pregunta");
+        }
+      })
+  }
 
   //Icons to use
   iconCube = iconos.faCube;
